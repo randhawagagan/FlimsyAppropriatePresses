3f778160d2bb32abf5df54ce48b3f32a969a1fba
Gagan Singh <randhawa_gagan@live.com>
2020-03-23 03:08:18 +0000

    WEFSDF

--- index.js
+++ index.js
@@ -1,19 +1,103 @@
-/*LINKED LISTS and 
-12--> 5 --> 8 --> null
+/*DOUBLY LINKED LISTS
 */
-// let linkedList = {
-//   head : {
-//     value: 12,
-//     next: {
-//       //pointer to next
-//       value: 5, 
-//       next: {
-//         value: 8,
-//         next : null
-//       }
-//     }
-//   }//
-// }
+// add a method remove() to the linked list that deletes a node to the specified index
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null, 
+      prev: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    newNode.next = this.head;
+     this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+        array.push(currentNode.value)
+        currentNode = currentNode.next
+    }
+    return array;
+  }
+  insert(index, value){
+    //Check for proper parameters;
+    if(index >= this.length) {
+      console.log('yes')
+      return this.append(value);
+    }
+    
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    const leader = this.traverseToIndex(index-1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters      
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new DoublyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+//this.printList();
+// myLinkedList.prepend(1);
+// myLinkedList.insert(2, 99);
+// myLinkedList.insert(20, 88);
+// myLinkedList.remove(2);
+
+/*SINGLY LINKED LISTS and 
+12--> 5 --> 8 --> null
+
 class Node{
   constructor(value){
     this.value = value;
@@ -113,6 +197,21 @@ LL.printList();
 
 LL.remove(2);
 LL.remove(2);
+*/
+// let linkedList = {
+//   head : {
+//     value: 12,
+//     next: {
+//       //pointer to next
+//       value: 5, 
+//       next: {
+//         value: 8,
+//         next : null
+//       }
+//     }
+//   }//
+// }
+
 
 /*
 Google question forst recurring character
