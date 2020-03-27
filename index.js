21b459f2f8248eb4871051c614a6a1179eff93da
Gagan Singh <randhawa_gagan@live.com>
2020-03-27 08:57:13 +0000

    ddddd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*DOUBLY LINKED LISTS
 */
-// add a method remove() to the linked list that deletes a node to the specified indes
+// add a method remove() to the linked list that deletes a node to the specified in
 
 class DoublyLinkedList {
   constructor(value) {
