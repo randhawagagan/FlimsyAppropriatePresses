42f33adb92b3606be7559c5be27e3054d9dd36d6
Gagan Singh <randhawa_gagan@live.com>
2020-04-12 04:24:06 +0000

    asfdbghngfds

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*DOUBLY LINKED LISTS
 */
-// add a method remove() to the linked list that deletes a node to the specified jjjj
+// add a method remove() to the linked list that deletes a node to the specified hgdfg
 class DoublyLinkedList {
   constructor(value) {
     this.head = {
