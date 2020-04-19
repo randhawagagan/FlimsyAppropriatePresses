5fe0418bc5e20b2fff9fd4129994002e053c4f17
Gagan Singh <randhawa_gagan@live.com>
2020-04-19 07:42:59 +0000

    cfgm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*DOUBLY LINKED LISTS
 */
-// add a method remove() to the linked list that deletes a node to the spec
+// add a method remove() to the linked list that deletes a node to the specfgfghj,
 class DoublyLinkedList {
   constructor(value) {
     this.head = {
