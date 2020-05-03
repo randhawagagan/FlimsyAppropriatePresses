228a0908e0908ec0cc5c1bcafe50dabd284f1fc6
Gagan Singh <randhawa_gagan@live.com>
2020-05-03 17:24:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*DOUBLY LINKED LISTS
 */
-// add a method remove() to the linked list that deletes a node to the speg
+// add a method remove() to the linked list that deletes a node to the speggggg
 class DoublyLinkedList {
   constructor(value) {
     this.head = {
